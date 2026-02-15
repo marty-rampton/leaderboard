a29ed46ff1c8452b4a5e2b33b41599d0283ced62
marty-rampton <martyrampton@gmail.com>
2026-02-14 23:55:28 -0600

    move database config to env vars using dotenv

--- be/src/db/index.ts
+++ be/src/db/index.ts
@@ -1,11 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { Pool } from "pg";
 
 export const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "leaderboard",
-  password: "passw0rd",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
 });
 
 pool.connect()
